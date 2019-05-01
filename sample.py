c2ccb80d1edb1343135945076e270586ac1164f3
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2019-05-01 08:39:07 -0400

    Add some filtering to the sentence sampling

--- sample.py
+++ sample.py
@@ -6,7 +6,7 @@ FILE_PATH = 'black.csv'
 OUTPUT_FILE_BASE = 'samples/sample_'
 
 def random_sample_trial(sentences, write_time, sample_count):
-    random_selection = random.choices(sentences, k = 10)
+    random_selection = random.sample(sentences, 10)
     output_file = OUTPUT_FILE_BASE + str(write_time) + '.txt'
     with open(output_file, mode='a') as out_file:
         out_file.write("SAMPLE " +  str(sample_count) + ":\n\n")
@@ -21,10 +21,15 @@ def main():
         csv_reader = csv.reader(csv_file, delimiter=',')
         line_count = 0
         for row in csv_reader:
-            sentences.append(row[1])
-            line_count += 1
+            sent = row[1].strip()
+            if (sent[-1] == '.' and "\n" not in sent):
+                sentences.append(sent)
+                line_count += 1
         print("Initial Sentence Lines: ", line_count)
     
+    sentences = list(set(sentences))
+    print("Num Unique Sentence Lines: ", len(sentences))
+
     write_time = datetime.datetime.now()
     for i in range(0, 10):
         random_sample_trial(sentences, write_time, i)
