eb0df59baf16d3c4414c8547882a6967a9786ef6
Bomani Oseni McClendon <bomanimc@gmail.com>
2019-11-06 00:59:02 -0500

    Fix spacing of text in help text

--- article_sents.py
+++ article_sents.py
@@ -2,9 +2,11 @@ import click
 import datetime
 from scrapers import webmd_article_scraper
 
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
 write_time = datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
 
-@click.command()
+@click.command(context_settings=CONTEXT_SETTINGS)
 @click.option(
     '-c', '--chromedriver-path', 'chromedriver_path',
     default = None,
@@ -16,8 +18,8 @@ write_time = datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
     default = None,
     required = True,
     help = (
-        f"Used to specify the path to the newline-delimited .txt file containing the"
-        f"links to the articles that this scraper should evaluated. The simplest" 
+        f"Used to specify the path to the newline-delimited .txt file containing the "
+        f"links to the articles that this scraper should evaluated. The simplest " 
         f"approach is to specify a file that was created by the Search Results Scraper."
     )
 )
@@ -26,10 +28,10 @@ write_time = datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
     default = 'data/test/webmd_sentences/article_sents_' + write_time + '.csv',
     show_default = True,
     help = (
-        f"Used to specify the path to the CSV file where the results will be stored." 
-        f"If the directory doesn't exist, it will be created. If an output file"
-        f"containing some results is specified, this program will skip links that"
-        f"have already been evaluated. This is a good technique to use if you need"
+        f"Used to specify the path to the CSV file where the results will be stored. " 
+        f"If the directory doesn't exist, it will be created. If an output file "
+        f"containing some results is specified, this program will skip links that "
+        f"have already been evaluated. This is a good technique to use if you need "
         f"to resume scraping after quitting the scraper before it finishes."
     )
 )
@@ -39,8 +41,8 @@ write_time = datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
     show_default = True,
     is_flag=True,
     help = (
-        f"Used to specify that the sentences collected from WebMD articles should be"
-        f"filtered to remove potentially irrelevant sentences based on the logic in"
+        f"Used to specify that the sentences collected from WebMD articles should be "
+        f"filtered to remove potentially irrelevant sentences based on the logic in "
         f"utils/filter_results.py."
     )
 )
--- search_results.py
+++ search_results.py
@@ -2,15 +2,17 @@ import click
 import datetime
 from scrapers import webmd_search_results_scraper
 
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
 write_time = datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
 
-@click.command()
+@click.command(context_settings=CONTEXT_SETTINGS)
 @click.option(
     '-o', '--output-folder', 'output_folder',
     default = 'data/test/search_result_links_' + write_time,
     show_default = True,
     help = (
-        f"Used for specifying which directory to place the output files"
+        f"Used for specifying which directory to place the output files "
         f"into. If the directory doesn't exist, it will be created."
     )
 )
