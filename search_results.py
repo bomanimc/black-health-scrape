c5593aaac71e916c94b8f41dd6dc28b46242adf0
Bomani Oseni McClendon <bomanimc@gmail.com>
2019-11-06 00:14:52 -0500

    Simplify search_results script with Click argument handling

--- search_results.py
+++ search_results.py
@@ -1,34 +1,19 @@
-import getopt
-import sys
+import click
 import datetime
 from scrapers import webmd_search_results_scraper
 
 write_time = datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
 
-# Default Configurations
-OUTPUT_FOLDER_PATH = 'data/test/search_result_links_' + write_time
+@click.command()
+@click.option(
+    '-o', '--output-folder', 'output_folder',
+    default = 'data/test/search_result_links_' + write_time,
+    show_default = True,
+    help = """Used for specifying which directory to place the 
+    output files into. If the directory doesn't exist, it will be created."""
+)
+def main(output_folder):
+    webmd_search_results_scraper.scrape_search_result_links(output_folder)
 
-full_cmd_arguments = sys.argv
-argument_list = full_cmd_arguments[1:]
-gnuOptions = ['output-folder=']
-
-try:
-    arguments, values = getopt.getopt(argument_list, "", gnuOptions)
-except getopt.error as err:
-    # output error, and return with an error code
-    print (str(err))
-    sys.exit(2)
-
-for currentArgument, currentValue in arguments:
-    if currentArgument in ("--output-folder"):
-        OUTPUT_FOLDER_PATH = currentValue
-
-def validateConfigValues():
-    if (OUTPUT_FOLDER_PATH == None):
-        sys.exit(2)
-
-def main():
-    validateConfigValues()
-    webmd_search_results_scraper.scrape_search_result_links(OUTPUT_FOLDER_PATH)
-
-main()
+if __name__ == '__main__':
+    main()
