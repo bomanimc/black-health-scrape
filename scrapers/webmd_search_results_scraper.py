a9f0d3c1083322cc4d853a19a7ee24d83a71e32f
Bomani Oseni McClendon <bomanimc@gmail.com>
2019-11-22 14:46:47 -0500

    Update search results scraper to produce a CSVs using Pandas

--- scrapers/webmd_search_results_scraper.py
+++ scrapers/webmd_search_results_scraper.py
@@ -7,6 +7,7 @@ line-by-line into a .txt file file.
 """
 
 import os
+import pandas as pd
 from requests_html import HTMLSession
 
 BASE_SEARCH_URL = 'https://www.webmd.com/search/search_results/default.aspx?query=african%20american&page='
@@ -15,33 +16,26 @@ def scrape_search_result_links(output_folder_path):
     session = HTMLSession()
     status_code = 200
     pagination_counter = 1
+    search_result_links = []
 
-    news_links_filename = output_folder_path + "/news_search_results.txt"
-    other_links_filename = output_folder_path + "/other_search_results.txt"
-    os.makedirs(os.path.dirname(news_links_filename), exist_ok=True)
-    os.makedirs(os.path.dirname(other_links_filename), exist_ok=True)
-    
-    with open(news_links_filename, "w+") as news_file:
-        with open(other_links_filename, "w+") as other_file:
-            while (status_code == 200):
-                resp = session.get(BASE_SEARCH_URL + str(pagination_counter))
-                status_code = resp.status_code
-                results = resp.html.find('.results-container', first=True)
-                
-                if (results == None):
-                    print("Ended link scraping on results page %d.\n" % (pagination_counter - 1))
-                    break
+    while (status_code == 200):
+        resp = session.get(BASE_SEARCH_URL + str(pagination_counter))
+        status_code = resp.status_code
+        results = resp.html.find('.results-container', first = True)
+        
+        if (results == None):
+            print("Ended link scraping on results page %d.\n" % (pagination_counter - 1))
+            break
+        
+        for _, link in enumerate(results.absolute_links):
+            search_result_links.append(link)
 
-                for _, link in enumerate(results.absolute_links):
-                    if ('/news/' not in link):
-                        other_file.write(link + "\n")
-                    else:
-                        news_file.write(link + "\n") 
+        if (pagination_counter % 10 == 0):
+            print("Added links up to results page %d.\n" % pagination_counter)
+        
+        pagination_counter += 1
 
-                if (pagination_counter % 10 == 0):
-                    print("Added links up to results page %d.\n" % pagination_counter)
-                
-                pagination_counter += 1
+    os.makedirs(os.path.dirname(output_folder_path), exist_ok=True)
 
-            news_file.close()
-            other_file.close()
+    search_result_df = pd.DataFrame(search_result_links, columns=['search_result_url'])
+    search_result_df.to_csv(output_folder_path + '.csv', index=False)
